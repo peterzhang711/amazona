8485f372c4d098b257b78777fd7f646547c451c9
Peter Zhang <raben4@hotmail.com>
2021-06-22 14:19:16 +1000

    save on github

---
+++ frontend/src/data.js
@@ -0,0 +1,67 @@
+ const data =  {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            image: './images/p1.jpeg',
+            price: 120,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            _id: '2',
+            name: 'Adidads Fit Shirt',
+            image: './images/p2.jpeg',
+            price: 100,
+            brand:'Nike',
+            rating: 4,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            _id: '3',
+            name: 'Lee suit Shirt',
+            image: './images/p3.jpeg',
+            price: 198,
+            brand:'Nike',
+            rating: 4,
+            numReviews: 120,
+            description: 'high quality product',
+        },
+        {
+            _id: '4',
+            name: 'Nike Slim Pants',
+            image: './images/p4.jpeg',
+            price: 110,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            _id: '5',
+            name: 'Jean top Shirt',
+            image: './images/p5.jpeg',
+            price: 150,
+            brand:'Nike',
+            rating: 5,
+            numReviews: 40,
+            description: 'high quality product',
+        },
+        {
+            _id: '6',
+            name: 'Puma Fit Pants',
+            image: './images/p6.jpeg',
+            price: 167,
+            brand:'Nike',
+            rating: 4.5,
+            numReviews: 102,
+            description: 'high quality product',
+        },
+  
+    ]
+}
+
+export default data
